c8e4ba34f6fcb644cd524e963a97fc73ccf27dbf
ndudnicz <ndudnicz@student.42.fr>
2017-12-07 17:51:13 +0100

    signaux handler done

--- Server.cpp
+++ Server.cpp
@@ -14,11 +14,11 @@
 
 /* STATIC VARIABLES ==========================================================*/
 
-unsigned int       Server::_nChild = 0;
-std::string const  Server::_SERVERNAME ="matt_daemon";
-std::string const  Server::_LOCKFILEDIR = "/var/lock/";
-std::string const  Server::_LOCKFILENAME = Server::_LOCKFILEDIR +Server::_SERVERNAME + ".lock";
-std::string const  Server::_LOGNAME = "matt_daemon";
+unsigned int		Server::_nChild = 0;
+std::string const	Server::_SERVERNAME ="matt_daemon";
+std::string const	Server::_LOCKFILEDIR = "/var/lock/";
+std::string const	Server::_LOCKFILENAME = Server::_LOCKFILEDIR +Server::_SERVERNAME + ".lock";
+std::string const	Server::_LOGNAME = "matt_daemon";
 
 /* CONSTRUCTORS ==============================================================*/
 Server::Server( void ) :
@@ -27,34 +27,33 @@ _socketMaster(0),
 _inetAddr(0),
 _port(0)
 {
-  this->_fdLock = open(Server::_LOCKFILENAME.c_str(), O_CREAT, 0666);
-  if (flock(this->_fdLock, LOCK_EX | LOCK_NB)) {
-      throw Server::AlreadyRunningException();
-  }
-  this->reporter = new Tintin_reporter(Server::_SERVERNAME);
-  this->reporter->info("Server Initialized");
-  signal(SIGHUP, &Server::signalHandler);
-  signal(SIGINT, &Server::signalHandler);
-  signal(SIGQUIT, &Server::signalHandler);
-  signal(SIGILL, &Server::signalHandler);
-  signal(SIGABRT, &Server::signalHandler);
-  signal(SIGFPE, &Server::signalHandler);
-  signal(SIGSEGV, &Server::signalHandler);
-  signal(SIGPIPE, &Server::signalHandler);
-  signal(SIGALRM, &Server::signalHandler);
-  signal(SIGTERM, &Server::signalHandler);
-  signal(SIGUSR1, &Server::signalHandler);
-  signal(SIGUSR2, &Server::signalHandler);
-  signal(SIGCHLD, &Server::signalHandler);
-  signal(SIGCONT, &Server::signalHandler);
-  signal(SIGTSTP, &Server::signalHandler);
-  signal(SIGTTIN, &Server::signalHandler);
-  signal(SIGTTOU, &Server::signalHandler);
-  signal(SIGKILL, &Server::signalHandler);
+	this->_fdLock = open(Server::_LOCKFILENAME.c_str(), O_CREAT, 0666);
+	if (flock(this->_fdLock, LOCK_EX | LOCK_NB)) {
+		throw Server::AlreadyRunningException();
+	}
+	this->reporter = new Tintin_reporter(Server::_SERVERNAME);
+	this->reporter->info("Server Initialized");
+	signal(SIGHUP, &Server::signalHandler);
+	signal(SIGINT, &Server::signalHandler);
+	signal(SIGQUIT, &Server::signalHandler);
+	signal(SIGILL, &Server::signalHandler);
+	signal(SIGABRT, &Server::signalHandler);
+	signal(SIGFPE, &Server::signalHandler);
+	signal(SIGSEGV, &Server::signalHandler);
+	signal(SIGPIPE, &Server::signalHandler);
+	signal(SIGALRM, &Server::signalHandler);
+	signal(SIGTERM, &Server::signalHandler);
+	signal(SIGUSR1, &Server::signalHandler);
+	signal(SIGUSR2, &Server::signalHandler);
+	signal(SIGCHLD, &Server::signalHandler);
+	signal(SIGCONT, &Server::signalHandler);
+	signal(SIGTSTP, &Server::signalHandler);
+	signal(SIGTTIN, &Server::signalHandler);
+	signal(SIGTTOU, &Server::signalHandler);
+	signal(SIGKILL, &Server::signalHandler);
 }
 
-Server::Server( Server const & src ) {
-}
+Server::Server( Server const & src ) {}
 
 Server::AlreadyRunningException::AlreadyRunningException(){}
 Server::AlreadyRunningException::AlreadyRunningException( AlreadyRunningException const &orig){
@@ -64,7 +63,7 @@ Server::SyscallException::SyscallException( void ) throw() {}
 Server::SyscallException::SyscallException( SyscallException const & src ) throw() {}
 
 /* MEMBER OPERATORS OVERLOAD =================================================*/
-Server    &Server::operator=( Server const & rhs ) {
+Server		&Server::operator=( Server const & rhs ) {
 	(void)rhs;
 	return *this;
 }
@@ -84,164 +83,192 @@ Server::AlreadyRunningException::~AlreadyRunningException( void ) {}
 */
 void
 Server::openConnection( void ) {
-  struct protoent      *proto;
-  struct sockaddr_in   sin;
-  // struct timeval       timeout;
-  int                  enable;
+	struct protoent			*proto;
+	struct sockaddr_in	 sin;
+	// struct timeval			 timeout;
+	int									enable;
 
-  enable = 1;
-  // timeout.tv_sec = 10;
-  // timeout.tv_usec = 0;
-  sin.sin_port = htons((unsigned short)DEFAULT_LISTENING_PORT);
-  // ntohs(sin.sin_port);
-  if ((proto = getprotobyname("tcp")) == 0) {
-    this->reporter->error(GPBN_FAIL);
-    throw Server::SyscallException();
-  }
-  if ((this->_socketMaster = socket(PF_INET, SOCK_STREAM, proto->p_proto)) < 0) {
-    this->reporter->error(SOCKET_FAILED);
-    throw Server::SyscallException();
-  }
-  if (setsockopt(this->_socketMaster, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
-    this->reporter->error(SETSOCKOPT_FAILED);
-    throw Server::SyscallException();
-  }
-  sin.sin_family = AF_INET;
-  if ((sin.sin_addr.s_addr = inet_addr(DEFAULT_BINDING_IP)) == INADDR_NONE) {
-    this->reporter->error(INET_ADDR_FAILED);
-    throw Server::SyscallException();
-  }
-  if (bind(this->_socketMaster, (const struct sockaddr*)&sin, sizeof(sin)) < 0) {
-    this->reporter->error(BIND_FAILED);
-    throw Server::SyscallException();
-  }
-  if (listen(this->_socketMaster, LISTEN_MAX) < 0) {
-    this->reporter->error(LISTEN_FAILED);
-    throw Server::SyscallException();
-  }
-  this->_inetAddr = sin.sin_addr.s_addr;
-  this->_port = sin.sin_port;
+	enable = 1;
+	// timeout.tv_sec = 10;
+	// timeout.tv_usec = 0;
+	sin.sin_port = htons((unsigned short)DEFAULT_LISTENING_PORT);
+	// ntohs(sin.sin_port);
+	if ((proto = getprotobyname("tcp")) == 0) {
+		this->reporter->error(GPBN_FAIL);
+		throw Server::SyscallException();
+	}
+	if ((this->_socketMaster = socket(PF_INET, SOCK_STREAM, proto->p_proto)) < 0) {
+		this->reporter->error(SOCKET_FAILED);
+		throw Server::SyscallException();
+	}
+	if (setsockopt(this->_socketMaster, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+		this->reporter->error(SETSOCKOPT_FAILED);
+		throw Server::SyscallException();
+	}
+	sin.sin_family = AF_INET;
+	if ((sin.sin_addr.s_addr = inet_addr(DEFAULT_BINDING_IP)) == INADDR_NONE) {
+		this->reporter->error(INET_ADDR_FAILED);
+		throw Server::SyscallException();
+	}
+	if (bind(this->_socketMaster, (const struct sockaddr*)&sin, sizeof(sin)) < 0) {
+		this->reporter->error(BIND_FAILED);
+		throw Server::SyscallException();
+	}
+	if (listen(this->_socketMaster, LISTEN_MAX) < 0) {
+		this->reporter->error(LISTEN_FAILED);
+		throw Server::SyscallException();
+	}
+	this->_inetAddr = sin.sin_addr.s_addr;
+	this->_port = sin.sin_port;
 }
 
 int
 Server::masterLoop(void) {
-  int                 pid;
-  struct sockaddr_in  csin;
-  unsigned int        cslen;
-  int                 newSocket;
+	int								 pid;
+	struct sockaddr_in	csin;
+	unsigned int				cslen;
+	int								 newSocket;
 
-  cslen = sizeof(csin);
-  while ((newSocket = accept(this->_socketMaster, (struct sockaddr*)&csin, &cslen)))
-  {
-    if (Server::_nChild < 3) {
-      pid = fork();
-      if (newSocket > 0 && pid == 0) {
-        /* CHILD */
-        std::cout << "exit child" << std::endl;
-        close(newSocket);
-        exit(0);
-        // while (1);
-      } else if (newSocket > 0 && pid) {
-        /* PARENT */
-        Server::_nChild += 1;
-        std::cout << "add child, nChild = " << Server::_nChild << std::endl;
-      }
-    } else {
-      std::cout << "nope, too many child :D" << std::endl;
-    }
-  }
-  close(this->_socketMaster);
-  return (0);
+	cslen = sizeof(csin);
+	while ((newSocket = accept(this->_socketMaster, (struct sockaddr*)&csin, &cslen)))
+	{
+		if (Server::_nChild < 3) {
+			pid = fork();
+			if (newSocket > 0 && pid == 0) {
+				/* CHILD */
+				std::cout << "exit child" << std::endl;
+				close(newSocket);
+				exit(0);
+				// while (1);
+			} else if (newSocket > 0 && pid) {
+				/* PARENT */
+				Server::_nChild += 1;
+				std::cout << "add child, nChild = " << Server::_nChild << std::endl;
+			}
+		} else {
+			std::cout << "nope, too many child :D" << std::endl;
+		}
+	}
+	close(this->_socketMaster);
+	return (0);
 }
 
 void
 Server::signalHandler( int sig ) {
-  int stat_loc;
-  std::cout << getpid() << " ";
-  switch (sig) {
-    case SIGHUP:
-    std::cout << LOG_SIGHUP << std::endl;
-    exit(0);
-    break;
-    case SIGINT:
-    std::cout << LOG_SIGINT << std::endl;
-    exit(0);
-    break;
-    case SIGQUIT:
-    std::cout << LOG_SIGQUIT << std::endl;
-    exit(0);
-    break;
-    case SIGILL:
-    std::cout << LOG_SIGILL << std::endl;
-    exit(0);
-    break;
-    case SIGABRT:
-    std::cout << LOG_SIGABRT << std::endl;
-    exit(0);
-    break;
-    case SIGFPE:
-    std::cout << LOG_SIGFPE << std::endl;
-    exit(0);
-    break;
-    case SIGSEGV:
-    std::cout << LOG_SIGSEGV << std::endl;
-    exit(0);
-    break;
-    case SIGPIPE:
-    std::cout << LOG_SIGPIPE << std::endl;
-    exit(0);
-    break;
-    case SIGALRM:
-    std::cout << LOG_SIGALRM << std::endl;
-    exit(0);
-    break;
-    case SIGTERM:
-    std::cout << LOG_SIGTERM << std::endl;
-    exit(0);
-    break;
-    case SIGUSR1:
-    std::cout << LOG_SIGUSR1 << std::endl;
-    exit(0);
-    break;
-    case SIGUSR2:
-    std::cout << LOG_SIGUSR2 << std::endl;
-    exit(0);
-    case SIGCHLD:
-    std::cout << LOG_SIGCHLD << std::endl;
-    wait(&stat_loc);
-    Server::_nChild -= Server::_nChild > 0 ? 1 : 0;
-    std::cout << "del child, nChild = " << Server::_nChild << std::endl;
-    break;
-    case SIGKILL:
-    std::cout << "KILLLL" << std::endl;
-    exit(0);
-    break;
-    case SIGSTKFLT:
-    std::cout << LOG_SIGSTKFLT << std::endl;
-    exit(0);
-    break;
-    case SIGURG:
-    std::cout << LOG_SIGURG << std::endl;
-    exit(0);
-    break;
-    case SIGXCPU:
-    std::cout << LOG_SIGXCPU << std::endl;
-    exit(0);
-    break;
-    case SIGXFSZ:
-    std::cout << LOG_SIGXFSZ << std::endl;
-    exit(0);
-    break;
-    default:
-    std::cout << "UNKNOWN" << std::endl;
-    exit(0);
-    break;
-  }
+	int stat_loc;
+	std::cout << getpid() << " ";
+	switch (sig) {
+		case SIGHUP:
+		std::cout << LOG_SIGHUP << std::endl;
+		exit(0);
+		break;
+		case SIGINT:
+		std::cout << LOG_SIGINT << std::endl;
+		exit(0);
+		break;
+		case SIGQUIT:
+		std::cout << LOG_SIGQUIT << std::endl;
+		exit(0);
+		break;
+		case SIGILL:
+		std::cout << LOG_SIGILL << std::endl;
+		exit(0);
+		break;
+		case SIGABRT:
+		std::cout << LOG_SIGABRT << std::endl;
+		exit(0);
+		break;
+		case SIGFPE:
+		std::cout << LOG_SIGFPE << std::endl;
+		exit(0);
+		break;
+		case SIGSEGV:
+		std::cout << LOG_SIGSEGV << std::endl;
+		exit(0);
+		break;
+		case SIGPIPE:
+		std::cout << LOG_SIGPIPE << std::endl;
+		exit(0);
+		break;
+		case SIGALRM:
+		std::cout << LOG_SIGALRM << std::endl;
+		exit(0);
+		break;
+		case SIGTERM:
+		std::cout << LOG_SIGTERM << std::endl;
+		exit(0);
+		break;
+		case SIGUSR1:
+		std::cout << LOG_SIGUSR1 << std::endl;
+		exit(0);
+		break;
+		case SIGUSR2:
+		std::cout << LOG_SIGUSR2 << std::endl;
+		exit(0);
+		case SIGCHLD:
+		std::cout << LOG_SIGCHLD << std::endl;
+		wait(&stat_loc);
+		Server::_nChild -= Server::_nChild > 0 ? 1 : 0;
+		std::cout << "del child, nChild = " << Server::_nChild << std::endl;
+		break;
+		case SIGKILL:
+		std::cout << "KILLLL" << std::endl;
+		exit(0);
+		break;
+		case SIGSTKFLT:
+		std::cout << LOG_SIGSTKFLT << std::endl;
+		exit(0);
+		break;
+		case SIGURG:
+		std::cout << LOG_SIGURG << std::endl;
+		exit(0);
+		break;
+		case SIGXCPU:
+		std::cout << LOG_SIGXCPU << std::endl;
+		exit(0);
+		break;
+		case SIGXFSZ:
+		std::cout << LOG_SIGXFSZ << std::endl;
+		exit(0);
+		break;
+		case SIGVTALRM:
+		std::cout << LOG_SIGVTALRM << std::endl;
+		exit(0);
+		break;
+		case SIGPROF:
+		std::cout << LOG_SIGPROF << std::endl;
+		exit(0);
+		break;
+		case SIGWINCH:
+		std::cout << LOG_SIGWINCH << std::endl;
+		exit(0);
+		break;
+		case SIGIO:
+		std::cout << LOG_SIGIO << std::endl;
+		exit(0);
+		break;
+		case SIGPWR:
+		std::cout << LOG_SIGPWR << std::endl;
+		exit(0);
+		break;
+		case SIGSYS:
+		std::cout << LOG_SIGSYS << std::endl;
+		exit(0);
+		break;
+		case SIGTRAP:
+		std::cout << LOG_SIGTRAP << std::endl;
+		exit(0);
+		break;
+		default:
+		std::cout << "UNKNOWN" << std::endl;
+		exit(0);
+		break;
+	}
 }
 
 const char*
 Server::SyscallException::what( void ) const throw() {
-  return ("");
+	return ("");
 }
 
 const char *
